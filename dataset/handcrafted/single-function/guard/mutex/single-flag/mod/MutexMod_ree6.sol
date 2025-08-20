1acbcb9f975c009234a919c5030bcc98c79569f1
DalilaRessi91 <dalila.ressi@unive.it>
2025-08-20 16:19:18 +0200

    sigle flag mod

---
+++ dataset/handcrafted/single-function/guard/mutex/single-flag/mod/MutexMod_ree6.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.0;
+
+// SPDX-License-Identifier: GPL-3.0
+contract C {
+    mapping (address => uint256) private balances;
+
+    bool private flag = false;
+
+    modifier nonReentrant() {   // broken mutex implemented via modifier
+        // missing require(!flag);
+        flag = true;
+        _;
+        // missing flag = false
+    }
+
+    function withdraw(uint256 amt) public nonReentrant {
+        require(balances[msg.sender] >= amt, "Insufficient funds");
+        (bool success, ) = msg.sender.call{value:amt}("");
+        require(success, "Call failed");
+        balances[msg.sender] -= amt;    // side effect after call
+    }
+
+    function deposit() public payable nonReentrant {
+        balances[msg.sender] += msg.value;       
+    }
+
+}
