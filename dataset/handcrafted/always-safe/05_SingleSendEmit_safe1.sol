689f521cb7641f1122148fb4f5f83ce9242e93a7
Alvise Spanò <spano.alvise@gmail.com>
2025-08-20 16:11:58 +0200

    Working on handcrafted

--- dataset/handcrafted/always-safe/05_SingleSendEmit_safe1.sol
+++ dataset/handcrafted/always-safe/05_SingleSendEmit_safe1.sol
@@ -4,15 +4,18 @@ pragma solidity ^0.8.0;
 contract C {
     mapping (address => uint256) public balances;
 
-    bool flag private = false;
+    event Sent(uint256 amt);
 
     function withdraw(uint256 amt) public {
-        require(!flag);
-
         require(balances[msg.sender] >= amt, "Insufficient funds");
-        (bool success, ) = msg.sender.call{value:amt}("");
-        require(success, "Call failed");
+        bool success1 = payable(msg.sender).send(amt);
+        require(success1, "Send failed");
         balances[msg.sender] -= amt;
+        emit Sent(amt);
+    }
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;       
     }
 
 }
