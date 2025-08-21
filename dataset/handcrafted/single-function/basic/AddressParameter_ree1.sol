fd7b08270b37f3e949af5449be4e343f2c6750d5
Alvise Spanò <spano.alvise@gmail.com>
2025-08-21 18:28:46 +0200

    Un altro sacco di roba

--- dataset/handcrafted/single-function/basic/AddressParameter_ree1.sol
+++ dataset/handcrafted/single-function/basic/AddressParameter_ree1.sol
@@ -4,13 +4,11 @@ pragma solidity ^0.8.0;
 contract C {
     mapping (address => uint256) public balances;
 
-    address private target = 0xD591678684E7c2f033b5eFF822553161bdaAd781;    // coin_base
-
-    function withdraw(uint256 amt) public {
+    function withdraw(address target, uint256 amt) public {
         require(balances[msg.sender] >= amt, "Insufficient funds");
-        (bool success, ) = target.call{value:amt}("");      // calls to a constant target address is potentially malicious
+        (bool success, ) = target.call{value:amt}("");    
         require(success, "Call failed");
-        balances[msg.sender] -= amt;    // side effect AFTER the call makes
+        balances[msg.sender] -= amt;    // side effect AFTER the call makes this contract vulnerable to reentrancy
     }
 
     function deposit() public payable {
