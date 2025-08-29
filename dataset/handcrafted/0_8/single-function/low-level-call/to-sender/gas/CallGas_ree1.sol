4b8df156b208a20e04182add4f0662bc7fa65be9
Alvise Spanò <spano.alvise@gmail.com>
2025-08-29 16:14:21 +0200

    New stuff again and again

--- dataset/handcrafted/0_8/single-function/low-level-call/to-sender/gas/CallGas_ree1.sol
+++ dataset/handcrafted/0_8/single-function/low-level-call/to-sender/gas/CallGas_ree1.sol
@@ -1,21 +1,15 @@
 pragma solidity ^0.8.0;
 
 // SPDX-License-Identifier: GPL-3.0
-
 contract C {
     mapping (address => uint256) public balances;
-
-    function pay(uint256 amt) public returns (bool) {
-        (bool b, ) = msg.sender.call{value: amt}("");
-        balances[msg.sender] = 0;    // side effect after call makes this vulnerable
-        return b;
-    }
 
     function withdraw() public {
         uint256 amt = balances[msg.sender];
         require(amt > 0, "Insufficient funds");
-        bool success = this.pay(amt);   // the compiler emits a CALL here, although it is never an external call as the pay() method above is always invoked
+        (bool success, ) = msg.sender.call{value:amt, gas:10000}("");    // the only way to make this vulnerable 
         require(success, "Call failed");
+        balances[msg.sender] = 0;   // side effect AFTER external call makes this unsafe because the attacker has enough gas to re-enter
     }
 
     function deposit() public payable {
