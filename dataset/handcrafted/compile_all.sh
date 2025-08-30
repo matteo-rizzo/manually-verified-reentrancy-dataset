a45121d8c299b71eb4d32520476655b70b96c4e0
Alvise Spanò <spano.alvise@gmail.com>
2025-08-30 14:45:26 +0200

    smartbugs results 0.8

--- dataset/handcrafted/compile_all.sh
+++ dataset/handcrafted/compile_all.sh
@@ -6,6 +6,6 @@ BASE_DIR="${1:-.}"
 # Trova tutti i file .sol ricorsivamente e invoca solc
 find "$BASE_DIR" -type f -name "*.sol" | while read -r file; do
     echo "Compilazione di: $file"
-    solcjs "$file" --bin -o bins   
+    solc "$file" --bin -o bins   
 done
 
