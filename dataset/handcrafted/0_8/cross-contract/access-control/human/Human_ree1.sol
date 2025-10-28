27325be47a8040165cec2ab09e17a7077788b0a7
Lorenzo Benetollo <lorenzo.benetollo@gmail.com>
2025-10-28 17:12:58 +0100

    Dataset Handcrafted Cross Contract Human Commented

--- dataset/handcrafted/0_8/cross-contract/access-control/human/Human_ree1.sol
+++ dataset/handcrafted/0_8/cross-contract/access-control/human/Human_ree1.sol
@@ -1,41 +1,43 @@
 pragma solidity ^0.8.0;
 
 // SPDX-License-Identifier: GPL-3.0
-contract C {
-    address[] private buyers;
 
-    uint public currentKeyPrice = 1;
-    address private lastBuyer;
-    uint private lastBuyTimestamp;
+// TODO This contract must be fixed cause this is not vulnerabile as it is now
+// contract C {
+//     address[] private buyers;
 
-    // this modifier checks that caller is an EOA, though it can be bypassed by a contract constructor
-    modifier isHuman() {
-        address _addr = msg.sender;
-        uint256 _codeLength;
-        assembly {_codeLength := extcodesize(_addr)}
-        require(_codeLength == 0, "sorry humans only");
-        _;
-    }
+//     uint public currentKeyPrice = 1;
+//     address private lastBuyer;
+//     uint private lastBuyTimestamp;
 
-    function buyKey(address refund_address) isHuman() public payable {
-        require(msg.value > currentKeyPrice, "Not enough to buy a key");
-        lastBuyer = msg.sender;
-        lastBuyTimestamp = block.timestamp;
-        currentKeyPrice = buyers.length * 100;
+//     // this modifier checks that caller is an EOA, though it can be bypassed by a contract constructor
+//     modifier isHuman() {
+//         address _addr = msg.sender;
+//         uint256 _codeLength;
+//         assembly {_codeLength := extcodesize(_addr)}
+//         require(_codeLength == 0, "sorry humans only");
+//         _;
+//     }
 
-        for (uint i = 0; i < buyers.length; i++) {
-            (bool success, ) = buyers[i].call{value:1}("");
-            require(success, "Refund failed");
-        }
-        buyers.push(refund_address);
-    }
+//     function buyKey(address refund_address) isHuman() public payable {
+//         require(msg.value > currentKeyPrice, "Not enough to buy a key");
+//         lastBuyer = msg.sender;
+//         lastBuyTimestamp = block.timestamp;
+//         currentKeyPrice = buyers.length * 100;
 
-    function close() isHuman public {
-        require(block.timestamp > lastBuyTimestamp + 1 hours);
-        (bool success, ) = lastBuyer.call{value: currentKeyPrice * 2}("");
-        require(success, "Call failed");
-    }
-}
+//         for (uint i = 0; i < buyers.length; i++) {
+//             (bool success, ) = buyers[i].call{value:1}("");
+//             require(success, "Refund failed");
+//         }
+//         buyers.push(refund_address);
+//     }
+
+//     function close() isHuman public {
+//         require(block.timestamp > lastBuyTimestamp + 1 hours);
+//         (bool success, ) = lastBuyer.call{value: currentKeyPrice * 2}("");
+//         require(success, "Call failed");
+//     }
+// }
 
 // off chain
 // Attacker att = new Attacker();
