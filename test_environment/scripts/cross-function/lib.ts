c01ae5500fb6b1656d11b93d7bc2806d25332aa6
Lorenzo Benetollo <lorenzo.benetollo@gmail.com>
2025-11-04 15:08:24 +0100

    Scripts refactoring

--- test_environment/scripts/cross-function/lib.ts
+++ test_environment/scripts/cross-function/lib.ts
@@ -1,50 +1,15 @@
 import { crossFunctionModuleBuilder, crossFunctionTrustSwapModuleBuilder } from "../../ignition/helpers/cross-function/cross-function-call.js";
 import { NetworkConnection } from "hardhat/types/network";
-import { printBalance } from "../lib.js";
+import { printBalance, runContractWithBuilder } from "../lib.js";
 
 export async function runCrossFunctionContracts(connection: NetworkConnection, contractNames: string[]) {
-    const [deployer, victim, victim2, attacker] = await connection.ethers.getSigners();
-
     for (const name of contractNames) {
-        const victimBalanceBefore = await connection.ethers.provider.getBalance(victim.address);
-        const victim2BalanceBefore = await connection.ethers.provider.getBalance(victim2.address);
-        const attackerBalanceBefore = await connection.ethers.provider.getBalance(attacker.address);
-
-        await deployCrossFunctionContract(connection, name);
-
-        const victimBalanceAfter = await connection.ethers.provider.getBalance(victim.address);
-        const victim2BalanceAfter = await connection.ethers.provider.getBalance(victim2.address);
-        const attackerBalanceAfter = await connection.ethers.provider.getBalance(attacker.address);
-
-        printBalance("Victim", victimBalanceBefore, victimBalanceAfter);
-        printBalance("Victim2", victim2BalanceBefore, victim2BalanceAfter);
-        printBalance("Attacker", attackerBalanceBefore, attackerBalanceAfter);
+        await runContractWithBuilder(connection, name, crossFunctionModuleBuilder);
     }
 }
 
 export async function runTrustSwapContracts(connection: NetworkConnection, contractNames: string[]) {
-    const [deployer, victim, victim2, attacker] = await connection.ethers.getSigners();
-
     for (const name of contractNames) {
-        const victimBalanceBefore = await connection.ethers.provider.getBalance(victim.address);
-        const victim2BalanceBefore = await connection.ethers.provider.getBalance(victim2.address);
-        const attackerBalanceBefore = await connection.ethers.provider.getBalance(attacker.address);
-
-        console.log(`Deploying and running: ${name}`);
-        await connection.ignition.deploy(crossFunctionTrustSwapModuleBuilder(name));
-
-        const victimBalanceAfter = await connection.ethers.provider.getBalance(victim.address);
-        const victim2BalanceAfter = await connection.ethers.provider.getBalance(victim2.address);
-        const attackerBalanceAfter = await connection.ethers.provider.getBalance(attacker.address);
-
-        printBalance("Victim", victimBalanceBefore, victimBalanceAfter);
-        printBalance("Victim2", victim2BalanceBefore, victim2BalanceAfter);
-        printBalance("Attacker", attackerBalanceBefore, attackerBalanceAfter);
+        await runContractWithBuilder(connection, name, crossFunctionTrustSwapModuleBuilder);
     }
-}
-
-
-export async function deployCrossFunctionContract(connection: NetworkConnection, name: string) {
-    console.log(`Deploying and running: ${name}`);
-    await connection.ignition.deploy(crossFunctionModuleBuilder(name));
 }
--- test_environment/scripts/single-function/low-level-call/to-sender/lib.ts
+++ test_environment/scripts/single-function/low-level-call/to-sender/lib.ts
@@ -1,50 +1,13 @@
-import { lowLevelToSenderModuleBuilder, twoStepsAttackerModuleBuilder } from "../../../../ignition/helpers/single-function/low-level-call.js";
-import { ethers } from "ethers";
 import { NetworkConnection } from "hardhat/types/network";
-import { printBalance } from "../../../lib.js";
+import { printBalance, runContractWithBuilder } from "../../../lib.js";
+import { lowLevelToSenderModuleBuilder, twoStepsAttackerModuleBuilder } from "../../../../ignition/helpers/single-function/low-level-call.js";
 
 export async function runSingleFunctionToSenderContracts(connection: NetworkConnection, contractNames: string[]) {
-    const [deployer, victim, victim2, attacker] = await connection.ethers.getSigners();
-
     for (const name of contractNames) {
-        const victimBalanceBefore = await connection.ethers.provider.getBalance(victim.address);
-        const victim2BalanceBefore = await connection.ethers.provider.getBalance(victim2.address);
-        const attackerBalanceBefore = await connection.ethers.provider.getBalance(attacker.address);
-
-        await deployLowLevelToSenderContract(connection, name);
-
-        const victimBalanceAfter = await connection.ethers.provider.getBalance(victim.address);
-        const victim2BalanceAfter = await connection.ethers.provider.getBalance(victim2.address);
-        const attackerBalanceAfter = await connection.ethers.provider.getBalance(attacker.address);
-
-        printBalance("Victim", victimBalanceBefore, victimBalanceAfter);
-        printBalance("Victim2", victim2BalanceBefore, victim2BalanceAfter);
-        printBalance("Attacker", attackerBalanceBefore, attackerBalanceAfter);
+        await runContractWithBuilder(connection, name, lowLevelToSenderModuleBuilder);
     }
 }
-
-export async function deployLowLevelToSenderContract(connection: NetworkConnection, name: string) {
-    console.log(`Deploying and running: ${name}`);
-    await connection.ignition.deploy(lowLevelToSenderModuleBuilder(name));
-}
-
 export async function runSingleFunctionToSenderBlockReeContract(connection: NetworkConnection, contractName: string) {
-    const [deployer, victim, victim2, attacker] = await connection.ethers.getSigners();
-
-
-    const victimBalanceBefore = await connection.ethers.provider.getBalance(victim.address);
-    const victim2BalanceBefore = await connection.ethers.provider.getBalance(victim2.address);
-    const attackerBalanceBefore = await connection.ethers.provider.getBalance(attacker.address);
-
-    console.log(`Deploying and running: ${contractName}`);
-    await connection.ignition.deploy(twoStepsAttackerModuleBuilder(contractName));
-
-    const victimBalanceAfter = await connection.ethers.provider.getBalance(victim.address);
-    const victim2BalanceAfter = await connection.ethers.provider.getBalance(victim2.address);
-    const attackerBalanceAfter = await connection.ethers.provider.getBalance(attacker.address);
-
-    printBalance("Victim", victimBalanceBefore, victimBalanceAfter);
-    printBalance("Victim2", victim2BalanceBefore, victim2BalanceAfter);
-    printBalance("Attacker", attackerBalanceBefore, attackerBalanceAfter);
+    await runContractWithBuilder(connection, contractName, twoStepsAttackerModuleBuilder);
 
 }
